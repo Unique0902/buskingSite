48b8ad21bf775c07d21d3076727ca07ccc905c76
unique0902 <rla0591@naver.com>
2023-11-24 14:11:10 +0900

    Fix: check build error sidebar

--- components/AppLayOut.jsx
+++ components/AppLayOut.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import AppHeader from './AppHeader';
 import ProtectedRoute from './ProtectedRoute';
-import SideBar from './SideBar';
 import UserDataProtectedRoute from './UserDataProtectedRoute';
 import { useMediaQuery } from 'react-responsive';
+import Sidebar from './SideBar';
 
 export default function AppLayOut({ children }) {
   const [isShowSideBar, setIsShowSideBar] = useState(true);
@@ -22,12 +22,12 @@ export default function AppLayOut({ children }) {
       <UserDataProtectedRoute>
         <section className='flex h-screen text-black bg-gradient-to-b from-blue-500 to-white '>
           {isShowSideBar && (
-            <SideBar
+            <Sidebar
               isShowSideBar={isShowSideBar}
               setIsShowSideBar={setIsShowSideBar}
             />
           )}
-          <main className=' grow py-6 px-8 max-lg:px-4 overflow-y-auto'>
+          <main className='px-8 py-6 overflow-y-auto  grow max-lg:px-4'>
             <AppHeader
               isShowSideBar={isShowSideBar}
               setIsShowSideBar={setIsShowSideBar}
