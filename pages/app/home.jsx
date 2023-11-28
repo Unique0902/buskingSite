302650074a2d66b726d25ba6466574a2720fa879
unique0902 <rla0591@naver.com>
2023-11-28 18:07:21 +0900

    Refactor: add noPlaylistCheckWrapper in appHome page

--- pages/app/home.jsx
+++ pages/app/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainSec from '../../components/MainSec';
+import NoPlaylistCheckWrapper from '../../components/NoPlaylistCheckWrapper';
 import { usePlaylistContext } from '../../context/PlaylistContext';
 import { useUserDataContext } from '../../context/UserDataContext';
 import { getAppLayOut } from '../../layouts/appLayout';
@@ -14,16 +15,12 @@ export default function AppHome() {
           <h1 className='mb-5 font-sans text-3xl font-bold text-black'>
             어서오세요! {userData.name} 님!
           </h1>
-          {nowPlaylist ? (
-            <h2 className='font-sans text-3xl font-normal text-black'>
-              현재 플레이리스트는 {nowPlaylist.name}입니다.
-            </h2>
-          ) : (
-            <h2 className='font-sans text-3xl font-bold text-black'>
-              현재 플레이리스트가 존재하지않습니다.
-            </h2>
-          )}
         </div>
+        <NoPlaylistCheckWrapper isExistWrapper={!!nowPlaylist}>
+          <h2 className='font-sans text-3xl font-normal text-black'>
+            현재 플레이리스트는 {nowPlaylist && nowPlaylist.name}입니다.
+          </h2>
+        </NoPlaylistCheckWrapper>
       </MainSec>
     </>
   );
