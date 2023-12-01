09140600f87abe8b58831c44a1f2231b99a199a5
unique0902 <rla0591@naver.com>
2023-12-01 18:59:55 +0900

    Fix: change heading elements descending order in TutorialBlock component

--- components/TutorialBlock.jsx
+++ components/TutorialBlock.jsx
@@ -20,9 +20,9 @@ const TutorialBlock = ({ imgAlt, title, isReverse = false, children }) => {
         <h2 className='font-sans text-5xl font-semibold text-center text-black max-lg:text-3xl'>
           {title}
         </h2>
-        <h4 className='font-sans text-xl font-normal text-center text-black max-lg:text-lg'>
+        <h3 className='font-sans text-xl font-normal text-center text-black max-lg:text-lg'>
           {children}
-        </h4>
+        </h3>
       </div>
     </section>
   );
