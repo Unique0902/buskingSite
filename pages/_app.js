cd81e8232053f99747bcb949dd918f22d3e19800
unique0902 <rla0591@naver.com>
2023-12-01 19:07:57 +0900

    Fix: add meta a description

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,12 @@ export default function App({ Component, pageProps }) {
                     당신만의 노래 리스트를 생성하여 애창곡을 검색하여 저장하세요.
                     버스킹을 시작하여 공유되는 링크를 이용하여 관객들로부터 노래를
                     신청받으세요.'
-        />
+        ></meta>
+        <meta
+          name='description'
+          content='당신만의 노래 리스트를 생성하세요.
+          노래책은 노래리스트를 여러 개 만들 수 있어요. 각 리스트마다 주제를 정해놓고 알맞은 곡을 저장해보세요.'
+        ></meta>
         <meta
           name='google-site-verification'
           content='cb3rxr8OeGSZq2ejzghLOqZ_6oRA3fSZWnJGbYcb9Wg'
