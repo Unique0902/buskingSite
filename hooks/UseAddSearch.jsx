fdbbca3c393898fc36bf86f3c9dd8831859cadcd
unique0902 <rla0591@naver.com>
2023-12-01 15:03:19 +0900

    Feat: add toptracks in add Page

--- hooks/UseAddSearch.jsx
+++ hooks/UseAddSearch.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLastFmContext } from '../context/LastFmContext';
 
 const useAddSearch = (setFilteredDataArr, setResultNum, nowPageNum) => {
   const [searchWord, setSearchWord] = useState({ name: '', category: '제목' });
   const [isLoading, setIsLoading] = useState(false);
-  const { searchSongByName, searchSongByArtist } = useLastFmContext();
+  const { searchSongByName, searchSongByArtist, getTopTracks } =
+    useLastFmContext();
 
   const search = async () => {
     setIsLoading(true);
     if (searchWord.name) {
       if (searchWord.category === '제목') {
         const result = await searchSongByName(searchWord.name, nowPageNum);
+        console.log(result);
         setFilteredDataArr(result.trackmatches.track);
         setResultNum(parseInt(result['opensearch:totalResults']));
       } else if (searchWord.category === '가수') {
@@ -21,6 +23,23 @@ const useAddSearch = (setFilteredDataArr, setResultNum, nowPageNum) => {
     }
     setIsLoading(false);
   };
+  const searchTopTrack = async () => {
+    setIsLoading(true);
+    const result = await getTopTracks(1);
+    console.log(result);
+    if (result.track) {
+      setFilteredDataArr(
+        result.track.map((data) => {
+          return { ...data, artist: data.artist.name };
+        })
+      );
+      setResultNum(parseInt(result['@attr'].totalPages));
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    searchTopTrack();
+  }, []);
 
   return [searchWord, setSearchWord, isLoading, search];
 };
