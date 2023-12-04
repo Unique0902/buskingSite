b17f046641565387bbbdf7298285b6112f8f185a
unique0902 <rla0591@naver.com>
2023-12-05 02:11:56 +0900

    Refactor: refactor songSearchBar to searchBar by compound design pattern in add page

---
+++ components/Search/SearchBar.tsx
@@ -0,0 +1,110 @@
+import React, { createContext, ReactNode } from 'react';
+import { SearchIcn } from '../../assets/icon/icon';
+import { color, xyPadding } from '../../styles/theme';
+import PrimaryBtn from '../Btn/PrimaryBtn';
+import RenderedWhenFullScreen from '../Responsive/RenderedWhenFullScreen';
+
+// const SearchBarContext = createContext(null);
+interface Props {
+  children: ReactNode;
+}
+
+const SearchBar = ({ children }: Props) => {
+  return (
+    <form
+      className='relative flex flex-row items-center justify-center w-full gap-2 mb-6 max-lg:flex-col'
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <div className='relative flex flex-row items-center justify-center flex-1 gap-2 max-lg:w-full max-lg:mx-2'>
+        {children}
+      </div>
+    </form>
+  );
+};
+
+type SearchWord = {
+  name: string;
+  category: '제목' | '가수';
+};
+type SelectProps = {
+  searchWord: SearchWord;
+  handleSelectChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  children: ReactNode;
+};
+const Select = ({ searchWord, handleSelectChange, children }: SelectProps) => {
+  return (
+    <select
+      className='p-2 text-lg border-2 border-black rounded-xl max-lg:mr-0 max-lg:text-base dark:bg-slate-500 '
+      value={searchWord.category}
+      onChange={handleSelectChange}
+    >
+      {children}
+    </select>
+  );
+};
+
+const Option = ({ value }: { value: string }) => {
+  return <option value={value}>{value}</option>;
+};
+
+type InputProps = {
+  inputValue: string;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleClickBtn: () => void;
+};
+
+// input 스타일링은 responsive 일정 브라우저 너비까지는 px로 유지하다가 그 px보다 작은 너비에서 그에 맞게 너비가 브라우
+//저 너비대비퍼센트로 줄어드는 로직으로 짜면 좋을듯 일정 너비 이하로 줄면 검색어 input이 사라지고 클릭시 input 보여주는 버
+//튼만 남기는것도 좋은 로직인듯
+const Input = ({
+  inputValue,
+  handleInputChange,
+  handleClickBtn,
+}: InputProps) => {
+  return (
+    <div className='relative flex flex-row items-center flex-1'>
+      <input
+        type='search'
+        className='w-full p-2 text-lg border-2 border-black rounded-xl max-lg:text-base dark:bg-slate-500'
+        placeholder='검색어를 입력하세요..'
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button
+        className='absolute right-4 lg:hidden'
+        onClick={handleClickBtn}
+        type='submit'
+      >
+        <SearchIcn width={18} height={18} color={color.gray_600} />
+      </button>
+    </div>
+  );
+};
+
+type ButtonProps = {
+  handleClickBtn: () => void;
+  text: string;
+};
+
+const Button = ({ handleClickBtn, text }: ButtonProps) => {
+  return (
+    <RenderedWhenFullScreen>
+      <PrimaryBtn
+        handleClick={handleClickBtn}
+        btnPadding={xyPadding.base}
+        isSubmit={true}
+      >
+        {text}
+      </PrimaryBtn>
+    </RenderedWhenFullScreen>
+  );
+};
+
+SearchBar.Select = Select;
+SearchBar.Option = Option;
+SearchBar.Input = Input;
+SearchBar.Button = Button;
+
+export default SearchBar;
--- pages/app/add.tsx
+++ pages/app/add.tsx
@@ -12,6 +12,7 @@ import LoadingCheckWrapper from '../../components/LoadingCheckWrapper';
 import NoPlaylistCheckWrapper from '../../components/NoPlaylistCheckWrapper';
 import useAddSearch from '../../hooks/UseAddSearch';
 import { FmEditedTopTrackData, FmTrackData } from '../../store/type/fm';
+import SearchBar from '../../components/Search/SearchBar';
 
 export default function AppAdd() {
   const [searchResults, setSearchResults] = useState<
@@ -52,12 +53,27 @@ export default function AppAdd() {
     searchBySearchBtn();
   };
 
+  const handleClickBtn = () => {
+    if (searchWord.category) {
+      onSearchBySearchBtn();
+    }
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchWord({ ...searchWord, name: e.target.value });
+  };
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearchWord({
+      ...searchWord,
+      category: e.target.value as '제목' | '가수',
+    });
+  };
+
   return (
     <>
       <TitleBar text={'노래추가'} />
       <NoPlaylistCheckWrapper isExistWrapper={!!nowPlaylist}>
         <MainSec>
-          <SongSearchBar
+          {/* <SongSearchBar
             searchWord={searchWord}
             setSearchWord={setSearchWord}
             onSearch={onSearchBySearchBtn}
@@ -74,7 +90,40 @@ export default function AppAdd() {
                 className='ml-3 text-2xl text-blue-500 max-lg:w-5 max-lg:h-5 max-lg:hidden'
               />
             </HoverIcon>
-          </SongSearchBar>
+          </SongSearchBar> */}
+
+          {/* compound 디자인패턴 이용한 리팩토링!! 좀더 가독성이 좋아진 기분이랄까 커스텀 변경도 쉬워짐!! */}
+          <div className='relative flex flex-row gap-4'>
+            {/* TODO:compound 디자인 관련해서는 고민해봐야될듯 atomic 디자인을 적용해볼까..?  */}
+            <SearchBar>
+              <SearchBar.Select
+                searchWord={searchWord}
+                handleSelectChange={handleSelectChange}
+              >
+                <SearchBar.Option value='제목' />
+                <SearchBar.Option value='가수' />
+              </SearchBar.Select>
+              <SearchBar.Input
+                inputValue={searchWord.name}
+                handleClickBtn={handleClickBtn}
+                handleInputChange={handleInputChange}
+              />
+              <SearchBar.Button handleClickBtn={handleClickBtn} text='검색' />
+            </SearchBar>
+            <HoverIcon
+              text={
+                'Api 특성상 제목, 가수명을 영어로 입력하시면 더 잘나옵니다.'
+              }
+            >
+              <InformIcn
+                color={'blue'}
+                width={24}
+                height={24}
+                className='ml-3 text-2xl text-blue-500 max-lg:w-5 max-lg:h-5 max-lg:hidden'
+              />
+            </HoverIcon>
+          </div>
+
           <LoadingCheckWrapper isLoading={isLoading}>
             <SongAddTable
               results={searchResults}
