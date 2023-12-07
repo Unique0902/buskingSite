22142eff497a512579535b685700d0afbd53b3a2
unique0902 <rla0591@naver.com>
2023-12-08 00:20:16 +0900

    Feat: add timeout 500 in all hoverBtn

--- components/Hover/HoverIcon.tsx
+++ components/Hover/HoverIcon.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { iconName } from '../../assets/icon/constants';
 import Icon from '../../assets/icon/icon';
@@ -8,7 +8,14 @@ type Props = {
 };
 const HoverIcon = ({ text, icon }: Props) => {
   const [isHovering, setIsHovering] = useState<boolean>(false);
-
+  const [isShowElement, setIsShowElement] = useState<boolean>(false);
+  useEffect(() => {
+    !isHovering && setIsShowElement(false);
+    const timer = isHovering
+      ? setTimeout(() => setIsShowElement(true), 100)
+      : undefined;
+    return () => clearTimeout(timer);
+  }, [isHovering]); // hovered가 변할 때만 작동하도록
   return (
     <div
       className='relative'
@@ -16,7 +23,7 @@ const HoverIcon = ({ text, icon }: Props) => {
       onMouseOut={() => setIsHovering(false)}
     >
       <Icon size={24} color='blue' icon={icon} />
-      {isHovering && (
+      {isShowElement && (
         <div className='absolute right-0 w-48 p-2 text-xs text-gray-600 bg-white border border-gray-500 rounded-lg'>
           {text}
         </div>
--- components/Hover/SlicedHoverText.tsx
+++ components/Hover/SlicedHoverText.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useMediaQuery } from 'react-responsive';
 type Props = {
@@ -6,6 +6,14 @@ type Props = {
 };
 const SlicedHoverText = ({ text }: Props) => {
   const [isHovering, setIsHovering] = useState<boolean>(false);
+  const [isShowElement, setIsShowElement] = useState<boolean>(false);
+  useEffect(() => {
+    !isHovering && setIsShowElement(false);
+    const timer = isHovering
+      ? setTimeout(() => setIsShowElement(true), 500)
+      : undefined;
+    return () => clearTimeout(timer);
+  }, [isHovering]); // hovered가 변할 때만 작동하도록
   const isPc = useMediaQuery({
     query: '(min-width:1024px)',
   });
@@ -19,7 +27,7 @@ const SlicedHoverText = ({ text }: Props) => {
       onMouseOut={() => setIsHovering(false)}
     >
       {slicedText}
-      {isHovering && (
+      {isShowElement && (
         <p className='absolute z-10 p-2 bg-gray-900 border border-gray-500 rounded-lg'>
           {text}
         </p>
